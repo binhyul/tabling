c3015ebc2b05a2c19ba96a0979ab831e4ab8c819
binhyul <gkswlssla@gmail.com>
2022-08-18 17:42:59 +0900

    feat: shop, shop tag component 초안

---
+++ app/src/main/java/com/example/tabling/component/ShopTagView.kt
@@ -0,0 +1,24 @@
+package com.example.tabling.component
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.res.ResourcesCompat
+import com.example.tabling.R
+
+class ShopTagView  @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    init {
+        background = AppCompatResources.getDrawable(context, R.drawable.bg_selectable_item)
+        setTextColor(ResourcesCompat.getColor(resources,R.color.blue,null))
+        val horizontalPadding = resources.getDimensionPixelOffset(R.dimen.shop_tag_horizontal_padding)
+        val verticalPadding = resources.getDimensionPixelOffset(R.dimen.shop_tag_vertical_padding)
+        setPadding(horizontalPadding,verticalPadding,horizontalPadding,verticalPadding)
+    }
+
+}
---
+++ app/src/main/java/com/example/tabling/component/ShopView.kt
@@ -0,0 +1,38 @@
+package com.example.tabling.component
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.tabling.R
+import com.example.tabling.databinding.ShopBinding
+import com.example.tabling.loadUrlImage
+
+class ShopView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    data class State(
+        val categories : List<String>,
+        val title: String,
+        val thumbnail: String,
+        val rating : Float,
+        val place : String,
+        val tags : List<String>
+    )
+
+    private val binding: ShopBinding =
+        ShopBinding.inflate(LayoutInflater.from(context), this)
+
+    init {
+        foreground = AppCompatResources.getDrawable(context, R.drawable.bg_selectable_item)
+    }
+
+    fun setState(state: State) {
+        binding.ivImg.loadUrlImage(state.thumbnail)
+        binding.tvTitle.text = state.title
+    }
+}
